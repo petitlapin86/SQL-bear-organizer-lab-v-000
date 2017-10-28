0da1d37f8085dedaef429c7a2d0e222e74d49b61
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-28 13:14:35 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'spicy', 1);
+VALUES ('Rowdy', 3, 'M', 'tawny', 'gentle', 1);
+VALUES ('Tabitha', 4, 'F', 'brown', 'shy', 1);
+VALUES ('Sergean Brown', 2, M, 'brown', 'gentle', 0);
+VALUES ('Melissa', 2, 'F', 'brown', 'shy', 0);
+VALUES ('Grinch', 3, 'M', 'orange', 'sweet', 0);
+VALUES ('Wendy', 2, 'F', 'tawny', 'sweet', 1);
+VALUES ('null', 2, M, 'tawny', 'angry', 1)
