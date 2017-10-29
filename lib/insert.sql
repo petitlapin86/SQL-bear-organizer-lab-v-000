23ea5bddcaf95357434b6761085a43a8168f2566
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-29 15:32:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Mr.Chocolate', 4, 'M', 'brown', 'spicy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Rowdy', 3, 'M', 'tawny', 'gentle', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Tabitha', 4, 'F', 'brown', 'shy', 1);
-VALUES ('Sergean Brown', 2, M, 'brown', 'gentle', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergean Brown', 2, 'M', 'brown', 'gentle', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Melissa', 2, 'F', 'brown', 'shy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Grinch', 3, 'M', 'orange', 'sweet', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ('Wendy', 2, 'F', 'tawny', 'sweet', 1);
-VALUES ('null', 2, M, 'tawny', 'angry', 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('null', 2, 'M', 'tawny', 'angry', 1);
